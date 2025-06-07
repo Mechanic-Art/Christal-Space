7ed129b4afb62da41335830dc6578fe433fb9f40
Christal1987 <christelle.fremat@gmail.com>
2025-06-07 15:36:46 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+   eleventyConfig.addPassthroughCopy("src/js");
+   eleventyConfig.addPassthroughCopy("src/style");
+   eleventyConfig.addPassthroughCopy("src/assets");
+   return {
+     dir: {
+       input: "src",
+       includes: "includes",
+       output: "_site"
+     },
+     htmlTemplateEngine: "njk"
+   };
+};
